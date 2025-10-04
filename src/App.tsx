b731b5669c61a083273a8328d78ef5ff3a1a2797
nydo(mac) <2337315348@qq.com>
2025-10-04 19:39:18 +1000

    路由懒加载

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { useRoutes } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { Link, useRoutes } from 'react-router-dom';
 import routes from '@/router';
 import Demo from './views/demo';
 
 function App() {
   return (
     <div className="App">
-      <div className="main">{useRoutes(routes)}</div>
+      <div className="nav">
+        <Link to="discover">发现音乐</Link>
+        <Link to="mine">我的音乐</Link>
+        <Link to="focus">关注</Link>
+        <Link to="download">下载客户端</Link>
+      </div>
+      {/* 这个 fallback 也可以写一个组件 */}
+      <Suspense fallback="loading...">
+        <div className="main">{useRoutes(routes)}</div>
+      </Suspense>
     </div>
   );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,15 @@
+import React, { lazy } from 'react';
 import type { RouteObject } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
-import Discover from '@/views/discover';
-import Mine from '@/views/mine';
-import Download from '@/views/download';
-import Focus from '@/views/focus';
+// 这几个都是独立组件, 可以做一个分包处理
+// webpack: 通过 import() 函数做分包
+// Vue/React: 路由的懒加载(路由)
+// 懒加载时, 有可能东西还没下载下来, 所以要用 Suspense 包裹
+const Discover = lazy(() => import('@/views/discover'));
+const Mine = lazy(() => import('@/views/mine'));
+const Download = lazy(() => import('@/views/download'));
+const Focus = lazy(() => import('@/views/focus'));
 
 const routes: RouteObject[] = [
   {
