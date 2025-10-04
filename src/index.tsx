3df13dc8816482f3c6cb18bd5ed25ac5d59c6bad
nydo(mac) <2337315348@qq.com>
2025-10-04 18:15:04 +1000

    css 样式重置, normalize.css + reset.less, 以及 less 的插件安装配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
+import '@/assets/css/index.less';
+import '@/assets/css/common.less';
+
 import App from '@/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
